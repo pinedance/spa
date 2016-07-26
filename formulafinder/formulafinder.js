2f412a4f868a6bf03cd6c0c41b8588a9311c487d
pinedance <pinedance@gmail.com>
2016-07-26 16:54:27 +0900

    add formula name search

--- formulafinder/formulafinder.js
+++ formulafinder/formulafinder.js
@@ -7,7 +7,7 @@ app.config(['$compileProvider', function ($compileProvider) {
 app.service('copyright', function(){
 	this.year = function(startYear){
 		var thisYear = new Date().getFullYear();
-		if (thisYear > startYear) { 
+		if (thisYear > startYear) {
 			return(startYear + '-' + thisYear);
 		} else {
 			return(thisYear);
@@ -17,53 +17,63 @@ app.service('copyright', function(){
 
 app.factory('loadSrc', ["$http", function($http){
 	return {
-		formulas: function(){ 
+		formulas: function(){
 			return $http.get('./src/sanghan_formulas.json')
 		}
 	}
 }])
 
 app.controller('FormulaCtrl', ['$scope', '$q', 'loadSrc', 'copyright', function($scope, $q, loadSrc, copyright) {
-			
+
 	////copyright
 	$scope.copyrightYear = copyright.year(2015);
 
 	var data, keys
-	
+
 	loadSrc.formulas().success(function(d, s){
 		data = d
 		keys = {
-			herbs: Object.keys(d.herbs), 
-			formulas: Object.keys(d.formulas), 
+			herbs: Object.keys(d.herbs),
+			formulas: Object.keys(d.formulas),
 			symptoms: Object.keys(d.symptoms)
 		}
-		$scope.gotData = true
+		$scope.gotData = true;
+    // initResults();
 	})
+
+  function initResults(){
+    $scope.results = keys.formulas;
+  }
 
 	$scope.loadItems = function(query, mykey) {
 		var deferred = $q.defer();
 		var filtered = keys[mykey].filter(function(s) { return s.indexOf(query) > -1; });
-		deferred.resolve( filtered.map( function(item){ 
+		deferred.resolve( filtered.map( function(item){
 			var result = new Object;
 			result[mykey] = item;
 			return result;
 		}) );
 		return deferred.promise;
-	};  // solution : http://stackoverflow.com/questions/23069562/autocomplete-using-ngtagsinput-cannot-read-property-then-of-undefined 
-	
+	};  // solution : http://stackoverflow.com/questions/23069562/autocomplete-using-ngtagsinput-cannot-read-property-then-of-undefined
+
 	$scope.find = function(){
 		let tmp_in = $scope.inHerbs.map(function(item) { return item.herbs });
 		let tmp_out = $scope.outHerbs.map(function(item) { return item.herbs });
-		
-		if ( (tmp_in.length + tmp_out.length)!==0 ){ $scope.results = []; return; }
+    let _formula = $scope.formulaName.map(function(item) { return item.formulas });
+    let tmp_formula = _formula.filter(function(item){ return keys.formulas.indexOf(item) > -1})
+
+		if ( (tmp_in.length + tmp_out.length)===0 ){
+      $scope.results = tmp_formula;
+      delete $scope.fmlIdx
+      return;
+    }
 
 		let handler = Object.keys(data.formulas);
-	
-		for (var i=0; i < tmp_in.length ; i++){ 
+		for (var i=0; i < tmp_in.length ; i++){
 			if ( data.herbs[tmp_in[i]] ){
 				handler = handler.intersection( data.herbs[tmp_in[i]].link );
 			} else {
-				$scope.results = []; return;
+				handler = handler.intersection( [] );
 			}
 		}
 		for (var j=0; j < tmp_out.length ; j++){
@@ -73,10 +83,14 @@ app.controller('FormulaCtrl', ['$scope', '$q', 'loadSrc', 'copyright', function(
 				continue;
 			}
 		}
-		$scope.results = handler;
+    if( tmp_formula && (tmp_formula.length > 0) ){
+        $scope.results = handler.intersection( tmp_formula )
+    } else {
+		    $scope.results = handler;
+    }
 		delete $scope.fmlIdx
 	};
-	
+
 	$scope.findHerbsymp = function(){
 		var _output = $scope.inSymp.map(function(item) { return data.symptoms[item.symptoms] });
 		var herbArr = _output.map(function(item){ return item.herbs })
@@ -88,48 +102,38 @@ app.controller('FormulaCtrl', ['$scope', '$q', 'loadSrc', 'copyright', function(
 		$scope.herbssymptoms = _outHerbArr
 	};
 
-	$scope.findFormula = function(){
-		console.log($scope.formulaName)
-		var _output = $scope.formulaName.map(function(item) { return item.formulas });
-		let handler = Object.keys(data.formulas);
-		$scope.resultes = _output.filter(function(item){ return handler.indexOf(item) > -1})
-	};
-
 	$scope.addHerb = function(hb){
 		var included = $scope.inHerbs.filter(function(herb){return herb.herbs == hb}) // 이미 있다면 넣지 않음 (ng-repeat 오류 회피)
 		if (included.length > 0){ return }
 		$scope.inHerbs.push( {herbs: hb} );
 		$scope.find()
 	};
-	
-	$scope.showDetail = function(formula, index){ 
+
+	$scope.showDetail = function(formula, index){
 		$scope.detail = data.formulas[formula];
 		$scope.herbdetails = Object.keys(data.formulas[formula].ingOrg);
 		$scope.yack = "약징 내용";
 		$scope.fmlIdx = index
 		delete $scope.yackIdx
 	};
-	
+
 	$scope.showHerbDetail = function(h, index){
 		$scope.yack = data.herbs[h].txYG;
 		$scope.yackIdx = index
 	};
-	
+
 	$scope.resetForms = function(){
 		delete $scope.inHerbs;
 		delete $scope.outHerbs;
 		delete $scope.results;
-		
+
 		delete $scope.detail;
 		delete $scope.yack;
 		delete $scope.herbdetails;
-		
+
 		delete $scope.inSymp;
 		delete $scope.symptoms;
 		delete $scope.herbssymptoms;
 	};
 
 }]);
-
-
-	
